9df91b569e6cb9f2538f1c8bc0f77431d5d47569
samikshya <samikshyakharel405@gmail.com>
2024-03-09 23:56:45 +1100

    checkout form edited

--- src/pages/cart/CheckoutForm.jsx
+++ src/pages/cart/CheckoutForm.jsx
@@ -5,14 +5,23 @@ import {
 } from "@stripe/react-stripe-js";
 import Stripe from "stripe";
 import { useState } from "react";
-import { Box, Button, Container, Grid, IconButton, Paper, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  IconButton,
+  Paper,
+  Stack,
+  Typography,
+} from "@mui/material";
 import CustomInput from "../../component/input/CustomInput";
 import CartproductCard from "./CartproductCard";
 import { useSelector } from "react-redux";
 import CheckoutProductcart from "./CheckoutProductCart";
 import { Link } from "react-router-dom";
-import EditIcon from '@mui/icons-material/Edit';
-import HttpsIcon from '@mui/icons-material/Https';
+import EditIcon from "@mui/icons-material/Edit";
+import HttpsIcon from "@mui/icons-material/Https";
 
 const inputs = [
   { name: "email", label: "Email", type: "email", required: true },
@@ -27,10 +36,21 @@ const CheckoutForm = () => {
   const [email, setEmail] = useState("");
   const clientSecret = import.meta.env.VITE_SECRET_KEY;
   const { cartItem } = useSelector((state) => state.cart);
-  // const { userInfo } = useSelector((state) => state.auth);
+  const { userInfo } = useSelector((state) => state.auth);
+  const initialState = {
+    email: userInfo.email,
+    phone: userInfo.phone,
+    address: "",
+  };
+  const [formData, setformData] = useState(initialState);
   const subTotal = cartItem.reduce((a, b) => {
     return a + b.price * b.quantity;
   }, 0);
+  const handleChange=(e)=>{
+    const {name,value} = e.target
+    setformData({...formData, [name]:value})
+
+  }
 
   const backendURL =
     "https://webhook.site/#!/view/37a4fc86-f6df-498b-a2f2-a29af0dd3cda";
@@ -43,12 +63,12 @@ const CheckoutForm = () => {
       setError(formSubmitError.message);
       return;
     }
-    const price = 100;
+    const price = subTotal+2.98;
     const stripeInstance = new Stripe(import.meta.env.VITE_SECRET_KEY, {
       apiVersion: "2023-08-16",
     });
     const paymentIntent = await stripeInstance.paymentIntents.create({
-      amount: price * 32 * 100,
+      amount: price  * 100,
       currency: "aud",
     });
 
@@ -70,69 +90,105 @@ const CheckoutForm = () => {
   return (
     <Container>
       <Grid container spacing={2} paddingTop={2}>
-      <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-      <Paper sx={{padding:'10px'}}>
-        {/* <Typography>Pay</Typography> */}
-        <Typography variant="h5">$ {subTotal+2.98}</Typography>
-        <Box sx={{display:'flex', justifyContent:'space-between', paddingTop:'20px'}}>
-        <h2>Order Summary</h2>
-        <Link to='/cart'><IconButton color="primary">Edit bag <EditIcon/></IconButton></Link>
-        </Box>
-        
-      
-              <Stack spacing={2} border={1} padding={2} >
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+          <Paper sx={{ padding: "10px" }}>
+            <Typography variant="h5">$ {subTotal + 2.98}</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                paddingTop: "20px",
+              }}
+            >
+              <h2>Order Summary</h2>
+              <Link to="/cart">
+                <IconButton color="primary">
+                  Edit bag <EditIcon />
+                </IconButton>
+              </Link>
+            </Box>
+            <Paper>
+              <Stack spacing={2} padding={2}>
                 {cartItem.map((item, index) => {
-                  return<Box  key={item.id + index}><CheckoutProductcart key={item.id + index} item={item} />
-                  {index !== cartItem.length - 1 && <hr />}
-                  </Box>
+                  return (
+                    <Box key={item.id + index}>
+                      <CheckoutProductcart key={item.id + index} item={item} />
+                      {index !== cartItem.length - 1 && <hr />}
+                    </Box>
+                  );
                 })}
               </Stack>
-      
-      <Box sx={{display:'flex', justifyContent:'space-between', padding:'20px 0'}}>
-        <Typography>Subtotal</Typography>
-        <h4>${subTotal}</h4>
-      </Box>
-      <hr />
-      </Paper>
+            </Paper>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "20px 0",
+              }}
+            >
+              <Typography>Subtotal</Typography>
+              <h4>${subTotal}</h4>
+            </Box>
+            
+
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "20px 0",
+              }}
+            >
+              <Typography>Shipping fee </Typography>
+              <h4>$2.98</h4>
+            </Box>
+            <hr />
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                padding: "20px 0",
+              }}
+            >
+              <h3>Total </h3>
+              <h4>${subTotal + 2.98}</h4>
+            </Box>
+          </Paper>
         </Grid>
 
-
         <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-          <Paper sx={{padding:'10px'}}>
-          <div>{error}</div>
+          <Paper sx={{ padding: "10px" }}>
+            <div>{error}</div>
 
-          <form onSubmit={handleFormSubmit} >
-            {inputs.map((input) => {
-              return (
-                <CustomInput
-                  key={input.name}
-                  {...input}
-                  // onChange={handleChange}
-                />
-              );
-            })}
+            <form onSubmit={handleFormSubmit}>
+              {inputs.map((input) => {
+                return (
+                  <CustomInput
+                    key={input.name}
+                    {...input}
+                    value={formData[input.name]}
+                    onChange={handleChange}
+                  />
+                );
+              })}
 
-            <PaymentElement />
-            <Button
-          variant="contained"
-          style={{
-            background: "black",
-            width: "100%",
-            padding: "10px 0px",
-            marginTop:'20px'
-          }}
-          className="btn"
-          type="submit"
-          
-        >
-         <HttpsIcon/>  Checkout securly 
-        </Button>
-
-          </form>
+              <PaymentElement />
+              <Button
+                variant="contained"
+                style={{
+                  background: "black",
+                  width: "100%",
+                  padding: "10px 0px",
+                  marginTop: "20px",
+                }}
+                className="btn"
+                type="submit"
+              >
+                <HttpsIcon /> Checkout securly
+              </Button>
+            </form>
           </Paper>
         </Grid>
       </Grid>
-      
     </Container>
   );
 };
